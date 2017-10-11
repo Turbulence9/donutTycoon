d2a103f8c26dcefc4316ecc87bf72ee441ed8fa3
Peter Sarkis <peter@Peters-MacBook-Pro.local>
2017-10-11 14:40:28 -0500

    adding cascading property foreign keys, delete button no side of item

--- migrations/20171010140211_createShop-Donuts.js
+++ migrations/20171010140211_createShop-Donuts.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('shops_donuts',(t) => {
     t.integer('shop_id').unsigned();
-    t.foreign('shop_id').references('shops.id');
+    t.foreign('shop_id').references('shops.id').onDelete('CASCADE');
     t.integer('donut_id').unsigned();
-    t.foreign('donut_id').references('donuts.id');
+    t.foreign('donut_id').references('donuts.id').onDelete('CASCADE');
   })
 };
 
